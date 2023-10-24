237ad59245fdd5202bb40894828995373053be33
Jaime1320 <jhc0001@alu.medac.es>
2023-10-24 08:53:25 +0200

    magia negra

--- PHP/Formularios/peliculas.php
+++ PHP/Formularios/peliculas.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <?php require '../Funciones/base_peliculas.php' ?>
+    <?php require '../BaseDatos/base_peliculas.php' ?>
 </head>
 
 <body>
--- PHP/Formularios/usuario.php
+++ PHP/Formularios/usuario.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Usuario</title>
-    <?php require '../Funciones/base_de_datos.php' ?>
+    <?php require '../BaseDatos/base_de_datos.php' ?>
 </head>
 <body>
     <?php
