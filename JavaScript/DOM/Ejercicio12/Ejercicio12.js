4e988cd2c6bdd881648e76244b82788d78278746
Jaime Hermana Caffarena <jhc0001@alu.medac.es>
2023-11-07 00:35:14 +0100

    bichillo

---
+++ JavaScript/DOM/Ejercicio12/Ejercicio12.js
@@ -0,0 +1,37 @@
+let botones = document.querySelectorAll("input");
+let caja = document.querySelector("div");
+let margensito = window.getComputedStyle(document.querySelector("div")).marginLeft;
+let ventana = window.innerWidth;
+console.log(parseInt(ventana));
+console.log(parseInt(margensito));
+botones[1].disabled = true;
+botones[0].disabled = false;
+botones[0].addEventListener("click",function(){
+    botones[1].disabled = false;
+    if(((parseInt(margensito) + parseInt(caja.offsetWidth)) + 30 )> parseInt(ventana)){
+        this.disabled = true;
+        caja.style.marginLeft = 0;
+        caja.style.right = 0;
+    }else{
+        document.querySelector("div").setAttribute("style","margin-Left:"+ margensito);
+    }
+    console.log(parseInt(margensito));
+    console.log(margensito);
+    margensito = (parseInt(margensito) + 30) + "px";
+    console.log(margensito);
+})
+
+botones[1].addEventListener("click",function(){
+    if(parseInt(margensito)<=0){
+        this.disabled = true;
+    }else{
+        botones[0].disabled = false;
+        margensito = (parseInt(margensito) - 20) + "px";
+        document.querySelector("div").setAttribute("style","margin-Left:"+ margensito);
+        console.log(margensito);
+    }
+})
+
+
+
+
---
+++ JavaScript/DOM/Ejercicio19/Ejercicio19.js
@@ -0,0 +1,42 @@
+// 19.- En HTML, crea una lista de 3 elementos y el botón “Añadir”. Al pulsar sobre el
+// botón, se le pide al usuario que meta una cadena de texto y se creará una nueva entrada
+// en la lista con el texto indicado
+document.querySelector("input").addEventListener("click", function () {
+    anadirAlFondo();
+})
+document.querySelectorAll("input")[1].addEventListener("click", function () {
+    let nuevoli = document.createElement('li');
+    let textoli = document.createTextNode(prompt("Introduzca un texto a añadir"));
+    nuevoli.appendChild(textoli);
+    let listado = document.querySelectorAll("li");
+    let posi = -3;
+    while (posi == -3) {
+        let tempposi = prompt("introduce una posicion valida");
+        if (isNaN(tempposi)) {
+            posi = -3;
+            alert("tus muertos");
+
+        } else {
+            if (tempposi < 1 || tempposi > listado.length + 1) {
+                posi = -3;
+            } else {
+                posi = tempposi - 1;
+                if (posi == 3) {
+                    anadirAlFondo(textoli);
+                } else {
+                    document.querySelector("ul").insertBefore(nuevoli, listado[posi]);
+                }
+            }
+        }
+
+    }
+
+})
+function anadirAlFondo(textoli = "") {
+    if (textoli=="") {
+        textoli = document.createTextNode(prompt("Introduzca un texto a añadir"));
+    }
+        let nuevoli = document.createElement('li');
+        nuevoli.appendChild(textoli);
+        document.querySelector("ul").appendChild(nuevoli);
+}
