16c22627a8aa5b26eb803715979227a0e96ca7c3
Jaime1320 <jhc0001@alu.medac.es>
2023-12-19 14:39:23 +0100

    formularios JS

--- JavaScript/Formularios/Ej3.js
+++ JavaScript/Formularios/Ej3.js
@@ -1,10 +1,12 @@
 document.querySelectorAll("input")[4].addEventListener("click", function(ev){
+    document.querySelector("textarea").value = "";
     ev.preventDefault();
     let opciones = document.querySelectorAll("input[type = 'checkbox']");
     for(i = 0 ; i < opciones.length; i++){
         if(opciones[i].checked){
             document.querySelector("textarea").value += opciones[i].value + "\n";
         }
+        
     }
 })
 
