92eb6202fe698863b8d5bf21b05546123d8a4e15
Jaime Hermana Caffarena <jhc0001@alu.medac.es>
2024-01-10 18:57:29 +0100

    Mis ejercicios de formularios

---
+++ JavaScript/Formularios/Ej7.js
@@ -0,0 +1,28 @@
+document.querySelector("input").addEventListener("click",function(ev){
+    
+    ev.preventDefault();
+
+    let texto = document.querySelector("textarea").value;
+    console.log(texto);
+    let midiv = document.createElement("div");
+
+    if(texto.length<= 50){
+        midiv.textContent = texto;
+        midiv.style.setProperty("color", "blue");
+        document.body.appendChild(midiv);
+    }else{
+        let textobueno = texto.split('',50);
+        textobueno.join('');
+        midiv.textContent = textobueno;
+        midiv.style.setProperty("color", "blue");
+        document.body.appendChild(midiv);
+    } 
+})
+
+document.querySelector("textarea").addEventListener("keydown",function(ev){
+    if(document.querySelector("textarea").value.length>100){
+        document.querySelector("textarea").disabled = true;
+    }else{
+        document.querySelector("textarea").disabled = false;
+    }
+})
